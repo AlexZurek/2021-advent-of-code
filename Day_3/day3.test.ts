7b9bd14ea8091c423bee21dca40f82603b4009f8
Alex Zurek <alex.zurek@atomicobject.com>
2021-12-04 08:53:16 -0500

    Finished Day 3 part 2

--- Day_3/day3.test.ts
+++ Day_3/day3.test.ts
@@ -1,26 +1,34 @@
-import { calcRates } from "./day3";
+import { calcP1Rates, calcP2Rates } from "./day3";
 
 describe("Day 3", () => {
-  describe("calcRates", () => {
-    it("should calculate gamma and epsilon rate properly with example", () => {
-      const report = [
-        [0, 0, 1, 0, 0],
-        [1, 1, 1, 1, 0],
-        [1, 0, 1, 1, 0],
-        [1, 0, 1, 1, 1],
-        [1, 0, 1, 0, 1],
-        [0, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1],
-        [1, 1, 1, 0, 0],
-        [1, 0, 0, 0, 0],
-        [1, 1, 0, 0, 1],
-        [0, 0, 0, 1, 0],
-        [0, 1, 0, 1, 0],
-      ];
+  const sampleReport = [
+    [0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 0],
+    [1, 0, 1, 1, 0],
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [0, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 0, 0, 1],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+  ];
 
-      const rates = calcRates(report);
+  describe("Part 1", () => {
+    it("should calculate gamma and epsilon rate properly with example", () => {
+      const rates = calcP1Rates(sampleReport);
       expect(rates.gamma).toEqual(22);
       expect(rates.epsilon).toEqual(9);
+    });
+  });
+
+  describe("Part 2", () => {
+    it("should calculate oxygen and co2 ratings", () => {
+      const rates = calcP2Rates(sampleReport);
+      expect(rates.oxygen).toEqual(23);
+      expect(rates.co2).toEqual(10);
     });
   });
 });
--- Day_3/day3.ts
+++ Day_3/day3.ts
@@ -12,30 +12,36 @@ function getData(): number[][] {
     });
 }
 
-export function calcRates(report: number[][]): {
+function buildColumns(report: number[][]): number[][] {
+  let dataByColumn: number[][] = [];
+
+  for (let i = 0; i < report[0].length; i++) {
+    let column: number[] = [];
+
+    for (let j = 0; j < report.length; j++) {
+      column.push(report[j][i]);
+    }
+
+    dataByColumn.push(column);
+  }
+
+  return dataByColumn;
+}
+
+export function calcP1Rates(report: number[][]): {
   gamma: number;
   epsilon: number;
 } {
-  let allGammaData: number[][] = [];
+  const columns = buildColumns(report);
 
-  for (let i = 0; i < report[0].length; i++) {
-    let gammaData: number[] = [];
-
-    for (let j = 0; j < report.length; j++) {
-      gammaData.push(report[j][i]);
-    }
-
-    allGammaData.push(gammaData);
-  }
-
-  const gammaBinaryNumber = allGammaData
+  const gammaBinaryNumber = columns
     .map((data) => {
       const analysis = countBy(Math.floor)(data);
       return analysis["0"] > analysis["1"] ? 0 : 1;
     })
     .join("");
 
-  const epsilonBinaryNumber = allGammaData
+  const epsilonBinaryNumber = columns
     .map((data) => {
       const analysis = countBy(Math.floor)(data);
       return analysis["0"] > analysis["1"] ? 1 : 0;
@@ -48,6 +54,49 @@ export function calcRates(report: number[][]): {
   };
 }
 
+export function calcP2Rates(report: number[][]): {
+  oxygen: number;
+  co2: number;
+} {
+  let possibleOxygenNumbers = report;
+  let possibleCO2Numbers = report;
+
+  let index = 0;
+
+  while (possibleOxygenNumbers.length > 1) {
+    const columns = buildColumns(possibleOxygenNumbers);
+    const data = columns[index];
+    const analysis = countBy(Math.floor)(data);
+    const filterNum = analysis["1"] >= analysis["0"] ? 1 : 0;
+
+    possibleOxygenNumbers = possibleOxygenNumbers.filter(
+      (num) => num[index] === filterNum
+    );
+    index++;
+  }
+
+  index = 0;
+
+  while (possibleCO2Numbers.length > 1) {
+    const columns = buildColumns(possibleCO2Numbers);
+    const data = columns[index];
+    const analysis = countBy(Math.floor)(data);
+    const filterNum = analysis["0"] <= analysis["1"] ? 0 : 1;
+
+    possibleCO2Numbers = possibleCO2Numbers.filter(
+      (num) => num[index] === filterNum
+    );
+    index++;
+  }
+
+  return {
+    oxygen: parseInt(possibleOxygenNumbers[0].join(""), 2),
+    co2: parseInt(possibleCO2Numbers[0].join(""), 2),
+  };
+}
+
 const data = getData();
-const rates = calcRates(data);
-console.log("answer to part 1:", rates.gamma * rates.epsilon);
+const ratesP1 = calcP1Rates(data);
+console.log("answer to part 1:", ratesP1.gamma * ratesP1.epsilon);
+const ratesP2 = calcP2Rates(data);
+console.log("answer to part 2:", ratesP2.oxygen * ratesP2.co2);
