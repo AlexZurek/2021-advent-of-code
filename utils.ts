d355759d304b6873e2926d06dde43587be4816e6
Alex Zurek <alex.zurek@atomicobject.com>
2021-12-04 09:03:20 -0500

    Document parsefile function

--- utils.ts
+++ utils.ts
@@ -1,5 +1,10 @@
 import { readFileSync } from "fs";
 
+/**
+ * Reads the data file for the given day of Advent of Code.
+ * @param dayNum The desired day (i.e. 1-25)
+ * @returns The file as a `string` array
+ */
 export function parseFile(dayNum: number): string[] {
   const data = readFileSync(`./Day_${dayNum}/data.txt`, "utf-8");
   return data.split("\n").filter((d) => d !== "");
